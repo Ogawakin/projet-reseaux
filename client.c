0cd43400355366f0c97cdd85cf62a39491670115
Ab2nour <briochekine@hotmail.fr>
2022-01-07 15:15:40 +0100

    Update client.c

--- client.c
+++ client.c
@@ -18,7 +18,7 @@ int main(int argc, char**argv) {
  	int sockfd;
 
  	char buffer[BUFFER_SIZE];
- 	char *serverAddr = "127.0.0.1";
+ 	char *adresse_serveur = "127.0.0.1";
 
  	sockfd = socket(AF_INET, SOCK_STREAM, 0);  
  	if (sockfd < 0) {
@@ -30,7 +30,7 @@ int main(int argc, char**argv) {
 
  	memset(&addr, 0, sizeof(addr));  
  	addr.sin_family = AF_INET;  
- 	addr.sin_addr.s_addr = inet_addr(serverAddr);
+ 	addr.sin_addr.s_addr = inet_addr(adresse_serveur);
  	addr.sin_port = PORT;     
  
  	if (connect(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
