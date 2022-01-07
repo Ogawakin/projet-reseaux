44f3f50b2ff7ccd58f5d974563ae8818a6e70e1c
Ogawakin <htriolet@ymail.com>
2022-01-07 18:13:10 +0100

    Update serveur.c

--- serveur.c
+++ serveur.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-
-#include <netinet/in.h>
-
-#include <arpa/inet.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <sys/select.h>
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include <sys/wait.h>
+#include <sys/time.h>
 
 #define PORT 2022
 #define BUFFER_SIZE 1024
